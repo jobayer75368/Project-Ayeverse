0aa2190039f795d04529269b909021f4feea129d
Jobayer75368 <jobayer30325@gmail.com>
2026-01-08 12:38:56 +0600

    project navbar js added

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,4 +11,24 @@ $(document).ready(function () {
     hideBtn.click(function () {
         navMenu.toggleClass("side_menu");
     })
+
+    $(".engineering_btn").click(function () {
+        $(".engineering_img").show()
+        $(".exp_img").hide()
+        $(".new_img").hide()
+    })
+    $(".view_all_btn").click(function () {
+        $(".all_img").show()
+    })
+    $(".new_project_btn").click(function () {
+        $(".new_img").show()
+        $(".exp_img").hide()
+        $(".engineering_img").hide()
+
+    })
+    $(".exp_btn").click(function () {
+        $(".exp_img").show()
+        $(".engineering_img").hide()
+        $(".new_img").hide()
+    })
 })
